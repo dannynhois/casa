0680ac5a069dad56d23c8b8cf301fa5a0567ca08
Liz Leaverton <egleaverton@gmail.com>
2017-10-07 10:51:02 -0500

    sharing for association debugging

--- controllers/home.js
+++ controllers/home.js
@@ -31,72 +31,47 @@ module.exports = function(app) {
       .then(function(houseData) {
         res.render("user", { houseData });
       });
-  });
-};
+  }); //closes get user
 
-// var db = require("../models");
 
-// app.get("/user/:id", function(req,res) {
-// 	db.House.findAll({
-// 		where:{
-// 			user_id: req.params.id;
-// 		}
-// 	})
-
-// 	res.render('user', {
-
-// 	})
-// })
-// module.exports = function(app) {
-// app.get("/user/:id", function(req, res) {
-//     db.House.findAll({
-//         where: {
-//             user_id: req.params.id
-//         }
-//     }).then(function(houseData) {
-//         res.render('user', {houseData});
-//     });
-// });
-
-// app.post("/user/:id", function(req, res) {
-// //     var parametersSearch = {
-// //         address: req.body.address,
-// //         citystatezip: req.body.city+ ", "+req.body.state+" "+zip
-// //     };
-// var zwsid = "X1-ZWz1fx6rub800b_55f1z";
-// var zillow = new Zillow(zwsid);
+app.post("/user/:id", function(req, res) {
+    console.log(req);
+    var parametersSearch = {
+        address: req.body.address,
+        citystatezip: req.body.city+ ", "+req.body.state+" "+req.body.zip
+    };
+var zwsid = "X1-ZWz1fx6rub800b_55f1z";
+var zillow = new Zillow(zwsid);
 // var parametersSearch = {
 //     address: "1903 Bradshaw St",
 //     citystatezip: "Houston, TX 77008"
 // };
-// var house = {};
-// zillow.get('GetDeepSearchResults', parametersSearch).then(function(results) {
-//     var resultsString = results.response.results.result;
-//     house.address = results.request.address;
-//     house.citystatezip = results.request.citystatezip;
-//     house.price = resultsString[0].zestimate[0].amount[0]._;
-//     house.zpid = resultsString[0].zpid;
-//     house.year = resultsString[0].yearBuilt;
-//     house.sqft = resultsString[0].finishedSqFt;
-//     house.bedrooms = resultsString[0].bedrooms;
+var house = {};
+zillow.get('GetDeepSearchResults', parametersSearch).then(function(results) {
+    var resultsString = results.response.results.result;
+    house.address = results.request.address;
+    house.citystatezip = results.request.citystatezip;
+    house.price = resultsString[0].zestimate[0].amount[0]._;
+    house.zpid = resultsString[0].zpid;
+    house.year = resultsString[0].yearBuilt;
+    house.sqft = resultsString[0].finishedSqFt;
+    house.bedrooms = resultsString[0].bedrooms;
 
-//     console.log("call 1" + JSON.stringify(house));
-// }).then(function() {
-//     console.log(house);
-//     db.House.create({
-//         user_id: "test5",
-//         address: house.address,
-//         citystatezip: house.citystatezip,
-//         zpid: house.zpid,
-//         sqft: house.sqft,
-//         bedrooms: house.bedrooms,
-//         yearbuilt: house.yearbuilt,
-//         zestimate: house.price
-//     });
-//     });
-//     // .then(function() {
-// //         res.render('user', dbHouses);
-// //     });
-
-// });
-// };
+    console.log("call 1" + JSON.stringify(house));
+}).then(function() {
+    console.log("posted house "+house);
+    db.House.create({
+        user_id: "test5",
+        address: house.address,
+        citystatezip: house.citystatezip,
+        zpid: house.zpid,
+        sqft: house.sqft,
+        bedrooms: house.bedrooms,
+        yearbuilt: house.yearbuilt,
+        zestimate: house.price
+    });
+    }).then(function(houseData) {
+        res.redirect("/user/:id");
+    });
+}); //closes post
+}; //closes module exports
