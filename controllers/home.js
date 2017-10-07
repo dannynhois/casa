9a576930f02b6bd5ab48bd5284abdd69fda70b97
Liz Leaverton <egleaverton@gmail.com>
2017-10-07 07:18:31 -0500

    adding get and post methods for house table

--- controllers/home.js
+++ controllers/home.js
@@ -2,34 +2,67 @@
  * GET /
  * Home page.
  */
-exports.index = (req, res) => {
-    res.render('index', {
-        title: 'Home'
-    });
-};
+// exports.index = (req, res) => {
+//     res.render('index', {
+//         title: 'Home'
+//     });
+// };
 
 
 var db = require("../models");
+var Zillow = require("node-zillow");
+
+
 module.exports = function(app) {
-    app.get("/user/:id", function(req, res) {
-        db.House.findAll({
-            where: {
-                user_id: req.params.id
-            }
-        }).then(function(dbHouses) {
-            res.render('user', dbHouses);
-        });
+app.get("/user/:id", function(req, res) {
+    db.House.findAll({
+        where: {
+            user_id: req.params.id
+        }
+    }).then(function(houseData) {
+        res.render('user', { houseData });
     });
+});
 
-    app.post("/user/:id", function(req, res) {
-        //call zillow
-        db.House.create({
-            where: {
-                user_id: req.params.id
-            }
-        }).then(function(dbHouses) {
-            res.render('user', dbHouses);
-        });
+app.post("/user/:id", function(req, res) {
+//     var parametersSearch = {
+//         address: req.body.address,
+//         citystatezip: req.body.citystatezip
+//     };
+var zwsid = "X1-ZWz1fx6rub800b_55f1z";
+var zillow = new Zillow(zwsid);
+var parametersSearch = {
+    address: "1903 Bradshaw St",
+    citystatezip: "Houston, TX 77008"
+};
+var house = {};
+zillow.get('GetDeepSearchResults', parametersSearch).then(function(results) {
+    var resultsString = results.response.results.result;
+    house.address = results.request.address;
+    house.citystatezip = results.request.citystatezip;
+    house.price = resultsString[0].zestimate[0].amount[0]._;
+    house.zpid = resultsString[0].zpid;
+    house.year = resultsString[0].yearBuilt;
+    house.sqft = resultsString[0].finishedSqFt;
+    house.bedrooms = resultsString[0].bedrooms;
+
+    console.log("call 1" + JSON.stringify(house));
+}).then(function() {
+    console.log(house);
+    db.House.create({
+        user_id: "test5",
+        address: house.address,
+        citystatezip: house.citystatezip,
+        zpid: house.zpid,
+        sqft: house.sqft,
+        bedrooms: house.bedrooms,
+        yearbuilt: house.yearbuilt,
+        zestimate: house.price
     });
+    });
+    // .then(function() {
+//         res.render('user', dbHouses);
+//     });
 
+});
 };
--- controllers/zillowapi.js
+++ controllers/zillowapi.js
@@ -4,8 +4,6 @@ var zwsid = "X1-ZWz1fx6rub800b_55f1z"
 var Zillow = require("node-zillow");
 var zillow = new Zillow(zwsid);
 
-var urlbase = "http://www.zillow.com/webservice/GetDeepSearchResults.htm";
-
 //data below is dummy data - this will be populated by the user eventually
 var address = "1903 Bradshaw St";
 var citystatezip = "Houston, TX 77008";
@@ -13,31 +11,18 @@ var citystatezip = "Houston, TX 77008";
 var house = {}
 
 var parametersSearch = {
-	address:address,
-	citystatezip:citystatezip
+    address: address,
+    citystatezip: citystatezip
 };
+zillow.get('GetDeepSearchResults', parametersSearch).then(function(results) {
+    var resultsString = results.response.results.result;
+    house.address = results.request.address;
+    house.price = resultsString[0].zestimate[0].amount[0]._;
+    house.zpid = resultsString[0].zpid;
+    house.year = resultsString[0].yearBuilt;
+    house.sqft = resultsString[0].finishedSqFt;
+    house.bedrooms = resultsString[0].bathrooms;
+    house.bathrooms = resultsString[0].bedrooms;
 
-zillow.get('GetDeepSearchResults',parametersSearch).then(function(results){
-	var resultsString = results.response.results.result;
-		house.address=results.request.address,
-		house.price=resultsString[0].zestimate[0].amount[0]._,
-		house.zpid=resultsString[0].zpid,
-		house.year=resultsString[0].yearBuilt,
-		house.sqft=resultsString[0].finishedSqFt,
-		house.bedrooms=resultsString[0].bathrooms,
-		house.bathrooms= resultsString[0].bedrooms
-
-	console.log("call 1"+JSON.stringify(house));
+    console.log("call 1" + JSON.stringify(house));
 });
-
-// var parametersImg = {
-// 	zwsid:zwsid,
-// 	zpid:27760273
-// };
-
-// zillow.get('GetUpdatedPropertyDetails',parametersImg).then(function(results){
-// 	console.log("call 2"+JSON.stringify(results));
-	
-// });
-
-
