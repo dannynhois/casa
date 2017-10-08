987455a1394927530e72931b12373ddf99110aef
Danny Nhoisaykham <danny@MBP.local>
2017-10-08 15:07:58 -0500

    format comment route code

--- controllers/comment.js
+++ controllers/comment.js
@@ -5,7 +5,6 @@ var db = require("../models");
 var Zillow = require("node-zillow");
 var middleware = require("./middleware");
 
-
 module.exports = function(app) {
   //add new comment
   app.post("/houses/:houseid/comments/", function(req, res) {
@@ -21,34 +20,39 @@ module.exports = function(app) {
       });
   });
 
-  
   //edit comment
   app.put("/houses/:houseid/comments/:commentid", function(req, res) {
-      db.Comment.update({
+    db.Comment
+      .update(
+        {
           comment: req.body.comment
-        }, {
-            where: {
-                id:req.params.commentid
-            }
-        }).then(function(){
-            console.log("comment changed successfully");
-            res.send("comment changed successfully");
+        },
+        {
+          where: {
+            id: req.params.commentid
+          }
+        }
+      )
+      .then(function() {
+        console.log("comment changed successfully");
+        res.send("comment changed successfully");
+      });
+  });
 
-        })
-    });
-    
-    app.delete("/houses/:houseid/comments/:commentid", function(req, res) {
-        db.Comment.destroy({
-            where: {
-                id: req.params.commentid
-            }
-        }).then(function(){
-            res.send("comment deleted");
-        })
-    });
+  app.delete("/houses/:houseid/comments/:commentid", function(req, res) {
+    db.Comment
+      .destroy({
+        where: {
+          id: req.params.commentid
+        }
+      })
+      .then(function() {
+        res.send("comment deleted");
+      });
+  });
 
-    //get comment
-    app.get("/houses/:houseid/comments/", function(req, res) {
-      res.send("got to comment get route");
-    });
+  //get comment
+  app.get("/houses/:houseid/comments/", function(req, res) {
+    res.send("got to comment get route");
+  });
 };
