aafbbec24c9ad1a5865b7b1c1ac3a13813242da8
Danny Nhoisaykham <danny@MBP.local>
2017-10-07 23:14:59 -0500

    add image scraper for zillow

---
+++ controllers/scraper.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var fs = require("fs");
+var request = require("request");
+var cheerio = require("cheerio");
+var app = express();
+
+var scraper = {}
+
+//url - passed from api
+scraper.scape = function(url, cb) {
+    //test url
+  //   var url =
+  //     "http://www.zillow.com/homedetails/9-Jennycliffe-Ln-Chesterfield-MO-63005/2779824_zpid/";
+
+  var imageLinks = [];
+
+  //headers needed to scrape zillow
+  var options = {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0",
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    url: url
+  };
+  request(options, function(error, response, html) {
+    //run if no error
+    if (!error) {
+      //load html
+      var $ = cheerio.load(html);
+
+      // notice photos has class hip-photo
+      // loop through all image url
+      // should change /p_c/ to /p_f/ for larger image
+      $(".hip-photo").filter(function() {
+        var imgUrl = $(this).attr("href");
+        // console.log(imgUrl);
+        imageLinks.push(imgUrl);
+      });
+    //   console.log("imglinks: ", imageLinks);
+    //call back
+    cb(imageLinks);
+    }
+  });
+
+}
+
+module.exports = scraper;
---
+++ controllers/testAPI.js
@@ -0,0 +1,46 @@
+// for zillow
+var Zillow = require("node-zillow");
+var _ = require("lodash");
+
+// for scraper
+var fs = require("fs");
+var request = require("request");
+var cheerio = require("cheerio");
+var scraper = require('./scraper');
+
+// for express
+var express = require("express");
+var app = express();
+
+app.get("/", (req, res) => {
+  var parametersSearch = {
+    address: "9 Jennycliffe Ln",
+    citystatezip: "Chesterfield, MO 63005"
+  };
+  var zwsid = "X1-ZWz1fx6rub800b_55f1z";
+  var zillow = new Zillow(zwsid);
+
+  var house = {};
+  // call api
+  zillow.get("GetDeepSearchResults", parametersSearch).then(function(data) {
+    // link to main page
+    house.link = data.response.results.result[0].links[0].homedetails[0];
+
+    //pass this url to scraper
+    var url = house.link;
+
+    //pass a url and scraper returns an array of image links. first is undefined for some reason
+    var a = scraper.scape(url, (x)=>{
+        console.log('a-url: ',x);
+    });
+
+
+
+    // url = http://www.zillow.com/homedetails/9-Jennycliffe-Ln-Chesterfield-MO-63005/2779824_zpid/
+    //aurl = https://www.zillow.com/homedetails/9-Jennycliffe-Ln-Chesterfield-MO-63005/2779824_zpid/
+  });
+});
+
+app.listen(8081, () => {
+  console.log("http://localhost:8081");
+});
