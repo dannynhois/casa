cffc83a496d75088dd5d691c315c9b96ff865a02
Danny Nhoisaykham <danny@MBP.local>
2017-10-08 15:07:13 -0500

    add comment put and delete route

--- controllers/comment.js
+++ controllers/comment.js
@@ -5,30 +5,50 @@ var db = require("../models");
 var Zillow = require("node-zillow");
 var middleware = require("./middleware");
 
+
 module.exports = function(app) {
   //add new comment
   app.post("/houses/:houseid/comments/", function(req, res) {
-    db.Comment.create({
+    db.Comment
+      .create({
         comment: req.body.comment,
         UserId: req.body.userid, // will need to be req.user.id once form is completed
         HouseId: req.params.houseid
-    }).then(function(){
-        console.log("comment added successfully")
+      })
+      .then(function() {
+        console.log("comment added successfully");
         res.send("comment added successfully");
-    })
+      });
   });
 
-  //edit comment
-  app.put("/houses/:houseid/comments/", function(req, res) {
-    res.send("got to comment put route");
-  });
-
+  
   //edit comment
   app.put("/houses/:houseid/comments/:commentid", function(req, res) {
-    res.send("got to comment put route");
-  });
+      db.Comment.update({
+          comment: req.body.comment
+        }, {
+            where: {
+                id:req.params.commentid
+            }
+        }).then(function(){
+            console.log("comment changed successfully");
+            res.send("comment changed successfully");
 
-  app.delete("/houses/:houseid/comments/:commentid", function(req, res) {
-    res.send("delete comment");
-  });
+        })
+    });
+    
+    app.delete("/houses/:houseid/comments/:commentid", function(req, res) {
+        db.Comment.destroy({
+            where: {
+                id: req.params.commentid
+            }
+        }).then(function(){
+            res.send("comment deleted");
+        })
+    });
+
+    //get comment
+    app.get("/houses/:houseid/comments/", function(req, res) {
+      res.send("got to comment get route");
+    });
 };
