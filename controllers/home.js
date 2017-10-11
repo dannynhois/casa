6073f783ba750657495845b64a7291a916cc9087
Danny Nhoisaykham <danny@MBP.local>
2017-10-10 19:55:00 -0500

    refactor api route into own file

--- controllers/home.js
+++ controllers/home.js
@@ -171,14 +171,6 @@ module.exports = function(app) {
       });
   }); //closes user post
 
-  //get route for modal call
-  app.get("/api/house/:houseid", (req, res) => {
-    db.House.findById(parseInt(req.params.houseid)).then(house => {
-      console.log(house);
-      res.json(house);
-    });
-  });
-
   //put route for modal call
   app.put("/houses", (req, res) => {
     db.House
